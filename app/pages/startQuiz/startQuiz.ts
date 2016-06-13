9d0f8568020f44bff4396e7758a4d6b9f77c3138
Mohammad Arsalan Rajput <a4arshi@yahoo.com>
2016-06-13 03:00:47 -0700

    ui-completed

--- app/pages/startQuiz/startQuiz.ts
+++ app/pages/startQuiz/startQuiz.ts
@@ -4,21 +4,28 @@ import {Page, NavController} from 'ionic-angular';
 import {OnInit} from '@angular/core';
 
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
-
+import {RADIO_GROUP_DIRECTIVES} from "ng2-radio-group";
 
 
 
 
 @Page({
-    templateUrl: 'build/pages/startQuiz/startQuiz.html'
+    templateUrl: 'build/pages/startQuiz/startQuiz.html',
+    directives: [RADIO_GROUP_DIRECTIVES]
 })
 export class startQuiz implements OnInit {
 
     data: FirebaseListObservable<any[]>;
     questionArr: any[] = [];
+    CheckboxOptionArray: any[] = [];
+    questionKeyArray: any[] = [];
     index: number = 0;
     question: any;
     lastQuestion: boolean = false;
+    correct: Boolean;
+    answer: String;
+    Quiz: any[] = [];
+    QuizQuestionSet: any[] = [];
     constructor(public nav: NavController, public af: AngularFire) { }
     ngOnInit() {
         this.af.database.list('quiz-in-progress/quiz01/').subscribe((res) => {
@@ -31,6 +38,7 @@ export class startQuiz implements OnInit {
                             for (var chapter in quizData[book].chapters) {
                                 for (var topic in quizData[book].chapters[chapter].topics) {
                                     for (var question in quizData[book].chapters[chapter].topics[topic].questions) {
+                                        this.questionKeyArray.push(question);
                                         this.questionArr.push(quizData[book].chapters[chapter].topics[topic].questions[question]);
                                     } // for in loop questions end
                                 } // for in loop Topics end
@@ -41,18 +49,78 @@ export class startQuiz implements OnInit {
               })// this.data For Each loop end
         })
      }
-     savequestion() {
+     savequestion(option,checked) {
+         console.log(this.CheckboxOptionArray)
+         if(checked){
+             this.CheckboxOptionArray.push(option);
+             console.log(option,checked)
 
+         }else {
+             console.log("unchecked")
+             this.CheckboxOptionArray.splice(this.CheckboxOptionArray.indexOf(option),1);
+
+         }
+         console.log(this.CheckboxOptionArray);
      }
-     nextQuestion() {
+     nextQuestion(optionRadioButton,question,QuestionSetOptionRadioButton) {
+         var questionIndex = this.questionArr.indexOf(question);
+        var questionKey = this.questionKeyArray[questionIndex]
+         if(question.type === 1) {
+             this.Quiz.push({
+                 html: question.html,
+                 type: question.type,
+                 option: optionRadioButton,
+                 questionKey: questionKey
+             })
+         }
+         else if(question.type === 2){
+             this.Quiz.push({
+                 html: question.html,
+                 type: question.type,
+                 option: this.CheckboxOptionArray,
+                 questionKey: questionKey
+             })
+         }
+         else  {
+            //  var val = questionSet.value
+             this.QuizQuestionSet = []
+             question.questiones.forEach((questionSet,i) =>{
+                 if(questionSet.type === 1){
+                    var questionRadioButton =  {
+                          html: questionSet.html,
+                          type: questionSet.type,
+                          option: QuestionSetOptionRadioButton,
+                      }
+                      this.QuizQuestionSet.push(questionRadioButton);
+                 }
+                 else if(questionSet.type === 2) {
+                    var questionCheckbox =  {
+                          html: questionSet.html,
+                          type: questionSet.type,
+                          option: this.CheckboxOptionArray,
+                      }
+                 this.QuizQuestionSet.push(questionCheckbox);
+
+                 }
+             })
+
+             this.Quiz.push({
+                 html: question.html,
+                 type: question.type,
+                 questiones: this.QuizQuestionSet,
+                 questionKey: questionKey
+             })
+         }
+         this.CheckboxOptionArray = [];
          this.index++;
-         console.log(this.questionArr.length -1 , this.index)
+
          if(this.questionArr.length > this.index) {
              this.question = this.questionArr[this.index];
 
          }
          if ((this.questionArr.length - 1)  == this.index) {
              this.lastQuestion = true;
+
          }
 
 
