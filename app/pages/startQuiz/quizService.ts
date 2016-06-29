dd17f2b0db3d74e3325bbdbc201a26c72610eb6e
Mohammad Arsalan Rajput <a4arshi@yahoo.com>
2016-06-29 12:13:25 +0500

    add user-quiz randomly

--- app/pages/startQuiz/quizService.ts
+++ app/pages/startQuiz/quizService.ts
@@ -73,9 +73,8 @@ export class QuizService {
             quiz.forEach(function(question, i) {
                 var questionKey = questionKeyArray[i]
                 var questionRandomIndex = i == 0 ? quiz.length - 1 : quiz.length - (i + 1);
-                console.log(questionRandomIndex);
                 // check if question.type == 1
-                if (question.type == 1) {
+                if (question.type == 1 || question.type == 2) {
                     // make object of quiz question radio button object
                     multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/correct"] = false;
                     multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/timer"] = 50;
@@ -85,21 +84,31 @@ export class QuizService {
                     })
                 }
                 // check if question.type == 2
-                if (question.type == 2) {
-                    // make object of quiz question checkbox object
-                    multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/correct"] = false;
-                    multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/timer"] = 50;
-                    question.options.forEach((option, optionIndex) => {
-                        var optionRandomIndex = optionIndex == 0 ? question.options.length - 1 : question.options.length - (optionIndex + 1);
-                        multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/options/" + optionRandomIndex + "/" + optionIndex + "/"] = option.html;
-                    })
-                }
+                // if (question.type == 2) {
+                //     // make object of quiz question checkbox object
+                //     multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/correct"] = false;
+                //     multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/timer"] = 50;
+                //     question.options.forEach((option, optionIndex) => {
+                //         var optionRandomIndex = optionIndex == 0 ? question.options.length - 1 : question.options.length - (optionIndex + 1);
+                //         multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/options/" + optionRandomIndex + "/" + optionIndex + "/"] = option.html;
+                //     })
+                // }
                 // check if question.type == 3
                 if (question.type == 3) {
                     question.questiones.forEach((questionSet, questionSetIndex) => {
                         var questionSetRandomIndex = questionSetIndex == 0 ? question.questiones.length - 1 : question.questiones.length - (questionSetIndex + 1);
-                        multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" + questionRandomIndex + "/" + questionKey + "/questiones/" + questionSetRandomIndex + "/" + questionSetIndex + "/html"] = questionSet.html;
+                        multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/correct"] = false;
+                        multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" +  questionRandomIndex + "/" + questionKey + "/timer"] = 50;
+                        // multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" + questionRandomIndex + "/" + questionKey + "/questiones/" + questionSetRandomIndex + "/" + questionSetIndex + "/html"] = questionSet.html;
                         multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" + questionRandomIndex + "/" + questionKey + "/questiones/" + questionSetRandomIndex + "/" + questionSetIndex + "/correct"] = false;
+                        console.log(questionSet,"questionSet")
+                        console.log(question,"questionSet")
+                        if(questionSet.type === 1 || questionSet.type === 2) {
+                            questionSet.options.forEach((questionSetOption, questionSetOptionIndex) => {
+                            var questionSetOptionRandomIndex = questionSetOptionIndex == 0 ? questionSet.options.length - 1 : questionSet.options.length - (questionSetOptionIndex + 1);
+                            multipathObject["answer-users/" + userId + "/" + groupId + "/" + subgroupId + "/" + quizId + "/questions/" + questionRandomIndex + "/" + questionKey + "/questiones/" + questionSetRandomIndex + "/" + questionSetIndex + "/options/" + questionSetOptionRandomIndex + "/" +  questionSetOptionIndex] = false;
+                            })
+                        }
 
                         // if (questionSet.type === 1) {
                         //     // make object of quiz questionSet radio button object
